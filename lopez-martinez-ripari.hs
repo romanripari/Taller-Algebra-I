f639222ff32cb4a8e5fbf24f2d552dae538a7313
Roman Lopez Garro <garrodev18@gmail.com>
2022-10-09 17:34:44 -0300

    Fixes: Typos y espaciado

--- lopez-martinez-ripari.hs
+++ lopez-martinez-ripari.hs
@@ -2,29 +2,33 @@
 -- Martínez, Fernando
 -- Ripari, Román Ariel
 
--- Ejercicio 1. sonCoprimos
+
+-- EJERCICIO 1: sonCoprimos
 sonCoprimos :: Integer -> Integer -> Bool
 sonCoprimos a b = mcd a b == 1
 
--- Auxiliares:
+-- AUXILIARES:
+
 mcd :: Integer -> Integer -> Integer
 mcd a b
  | b == 0 = a
  | a > b = mcd b (mod a b)
  | otherwise = mcd a (mod b a)
--- Devuelve el minimo comun divisor implementando el algoritmo de Euclides.
+-- Devuelve el máximo común divisor implementando el algoritmo de Euclides.
 
--- Ejercicio 2. es2Pseudoprimo 
+
+-- EJERCICIO 2: es2Pseudoprimo 
 es2Pseudoprimo :: Integer -> Bool
 es2Pseudoprimo p = esKPseudoprimo 2 p
 
--- Auxiliares:
+-- AUXILIARES:
+
 esKPseudoprimo :: Integer -> Integer -> Bool
 esKPseudoprimo k n
- | mod calculo n == 0 && not (esPrimo n)  = True
+ | mod calculo n == 0 && not (esPrimo n) = True
  | otherwise = False
   where calculo = (k ^ (n-1)) - 1
--- Devolverá True si es divisor del "cálculo" expresado en el where y si es compuesto (no primo)
+-- Devolverá True si n es divisor del "cálculo" expresado en el where y si es compuesto (no primo)
 
 esPrimo :: Integer -> Bool
 esPrimo 0 = False
@@ -39,33 +43,36 @@ primerDivisorDesde dividendo divisor
  | mod dividendo divisor == 0 = divisor
  | otherwise = primerDivisorDesde dividendo (divisor+1)
 
--- Ejercicio 3. cantidad3Pseudoprimos 
+
+-- EJERCICIO 3: cantidad3Pseudoprimos 
 cantidad3Pseudoprimos :: Integer -> Integer
 cantidad3Pseudoprimos n
  | n == 1 = 0
  | es3Pseudoprimo n = 1 + cantidad3Pseudoprimos (n-1)
  | otherwise = cantidad3Pseudoprimos (n-1)
 
--- Auxiliares:
+-- AUXILIARES:
 es3Pseudoprimo :: Integer -> Bool
 es3Pseudoprimo p = esKPseudoprimo 3 p
 
--- Ejercicio 4
+
+-- EJERCICIO 4. kesimo2y3Pseudoprimo
 kesimo2y3Pseudoprimo :: Integer -> Integer
 kesimo2y3Pseudoprimo k = kesimo2y3PseudoprimoDesde k 3
 
--- Auxiliares:
+-- AUXILIARES:
 kesimo2y3PseudoprimoDesde :: Integer -> Integer -> Integer
 kesimo2y3PseudoprimoDesde k n
  | k == 0 = (n-1)
  | es2Pseudoprimo (n) && es3Pseudoprimo (n) = kesimo2y3PseudoprimoDesde (k-1) (n+1)
  | otherwise = kesimo2y3PseudoprimoDesde k (n+1)
 
--- Ejercicio 5
+
+-- EJERCICIO 5. esCarmichael
 esCarmichael :: Integer -> Bool
 esCarmichael n = esCarmichaelDesde n 1
 
--- Auxiliares:
+-- AUXILIARES:
 esCarmichaelDesde :: Integer -> Integer -> Bool
 esCarmichaelDesde n base
  | base >= n = True
