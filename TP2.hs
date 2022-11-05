8aff3b5e6b910f49f776fe2cd15b144e943f6284
Roman Ripari <romanripari@gmail.com>
2022-11-05 18:24:26 -0300

    Falta 3era ej

--- TP2.hs
+++ TP2.hs
@@ -1,55 +1,116 @@
 -- Apellido Nombre email #1
 -- Apellido Nombre email #2
 -- Apellido Nombre email #3
-
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 type Complejo = (Float,Float)
 
+c1 :: Complejo
+c1 = (10.0, 5.0)
+c2 :: Complejo
+c2 = (5.0, 3.0)
 
--- 1.1
 re :: Complejo -> Float
+re z = fst z
 
--- 1.2
 im :: Complejo -> Float
+im z = snd z
 
--- 1.3
 suma :: Complejo -> Complejo -> Complejo
+suma (a, b) (c, d) = (a + c, b + d)
 
--- 1.4
 producto :: Complejo -> Complejo -> Complejo
+producto (a, b) (c, d) = ((a * c - b * d), (a * d + b * c))
 
--- 1.5
 conjugado :: Complejo -> Complejo
+conjugado (a, 0) = (a, 0)
+conjugado (a, b) = (a, -b)
 
--- 1.6
-inverso :: Complejo -> Complejo
-
--- 1.7
-cociente :: Complejo -> Complejo -> Complejo
-
--- 1.8
-potencia :: Complejo -> Integer -> Complejo
-
--- 1.9
-raicesCuadratica :: Float -> Float -> Float -> (Complejo,Complejo)
-
--- 2.1
 modulo :: Complejo -> Float
+modulo (a, b) = sqrt (a*a + b*b)
 
--- 2.2
+inverso :: Complejo -> Complejo
+inverso z =  ((fst z') / modCuadrado,
+              (snd z') / modCuadrado)
+ where z' = conjugado z
+       modCuadrado = (modulo z) * (modulo z)
+
+cociente :: Complejo -> Complejo -> Complejo
+cociente z z' = producto z (inverso z')
+
+potencia :: Complejo -> Integer -> Complejo
+potencia z 1 = z
+potencia z k = producto z (potencia z (k-1))
+
+raicesCuadratica :: Float -> Float -> Float -> (Complejo,Complejo)
+raicesCuadratica a b c = (z1, z2)
+ where z1 = cociente (suma (-b , 0) (conjugado w) ) (2 * a, 0)     
+       z2 = cociente (suma (-b , 0) w) (2 * a, 0) 
+       w = calculaDeterminante (b*b - 4 * a * c)
+
+calculaDeterminante :: Float -> Complejo
+calculaDeterminante a 
+ | a >= 0 =  (sqrt a, 0)
+ | a < 0 =  (0,  sqrt (-a))
+
+opuesto :: Complejo -> Complejo
+opuesto (a, b) = (-a, -b) 
+
 distancia :: Complejo -> Complejo -> Float
+distancia z w = modulo (suma z (opuesto w))
 
--- 2.3
 argumento :: Complejo -> Float 
+argumento (0, 0) = 0
+argumento (a, 0) 
+ | a < 0 = pi
+ | otherwise = 0 
+argumento (0, b)
+ | b < 0 = pi * 3/2
+ | otherwise = pi * 1/4 
+argumento (a, b)
+ | a > 0 && b > 0 = θ
+ | a < 0 && b < 0 = θ + pi
+ | a > 0 && b < 0 = θ + pi * 1/2
+ | a < 0 && b > 0 = θ + pi * 3/2
+ where θ = atan(b/a)
 
--- 2.4
 pasarACartesianas :: Float -> Float -> Complejo
+pasarACartesianas r θ = (a, b)
+ where a = r * cos θ
+       b = r * sin θ
 
--- 2.5
 raizCuadrada :: Complejo -> (Complejo,Complejo)
+raizCuadrada (a, b)
+ | calculo > 0 = mismoSigno x y  
+ | otherwise = distintoSigno x y  
+ where calculo = 2 * x * y
+       x = sqrt ((modulo (a, b) + a ) / 2 )
+       y = sqrt ((modulo (a, b) - a ) / 2 )
+
+mismoSigno :: Float -> Float -> (Complejo,Complejo)
+mismoSigno x 0 = ((x, 0), (-x, 0))
+mismoSigno 0 y = ((0, y), (0, -y))
+mismoSigno x y = ((x,y), (-x, -y))
+
+distintoSigno :: Float -> Float -> (Complejo,Complejo)
+distintoSigno x 0 = ((x, 0), (-x, 0))
+distintoSigno 0 y = ((0, y), (0, -y))
+distintoSigno x y = ((x,-y), (-x, y))
+
+productoRealComplejo :: Float -> Complejo -> Complejo
+productoRealComplejo real complejo = (real * re complejo, real * im complejo)
+
+
+raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo,Complejo)
+raicesCuadraticaCompleja a b c = (z1, z2)
+ where z1 = cociente (suma (opuesto b) (fst raices)) (productoRealComplejo 2 a)     
+       z2 = cociente (suma (opuesto b) (snd raices)) (productoRealComplejo 2 a)
+       raices = raizCuadrada (suma (potencia b 2) (opuesto (productoRealComplejo 4 (producto a c))))
+
+
+{-
 
 -- 2.6
-raicesCuadraticaCompleja :: Complejo -> Complejo -> Complejo -> (Complejo,Complejo)
 
 -- 3.1
 raicesNEsimas :: Integer -> [Complejo]
@@ -57,3 +118,4 @@ raicesNEsimas :: Integer -> [Complejo]
 -- 3.2
 sonRaicesNEsimas :: Integer -> [Complejo] -> Float -> Bool
 
+-}
